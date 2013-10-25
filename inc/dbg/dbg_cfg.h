f6fcc8b0ef57f1969c6c7b41d8b180757df3944b
Nenad Radulović <nenad.b.radulovic@gmail.com>
2013-10-26 00:47:14 +0200

    Some sane configuration

--- inc/dbg/dbg_cfg.h
+++ inc/dbg/dbg_cfg.h
@@ -1,22 +1,20 @@
 /*
- * This file is part of eSolid-Kernel
+ * This file is part of eSolid
  *
  * Copyright (C) 2011, 2012 - Nenad Radulovic
  *
- * eSolid-Kernel is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
+ * eSolid is free software; you can redistribute it and/or modify it under the
+ * terms of the GNU General Public License as published by the Free Software
+ * Foundation; either version 2 of the License, or (at your option) any later
+ * version.
  *
- * eSolid-Kernel is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
+ * eSolid is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
  *
- * You should have received a copy of the GNU General Public License
- * along with eSolid-Kernel; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin St, Fifth Floor,
- * Boston, MA  02110-1301  USA
+ * You should have received a copy of the GNU General Public License along with
+ * eSolid; if not, write to the Free Software Foundation, Inc., 51 Franklin St,
+ * Fifth Floor, Boston, MA  02110-1301  USA
  *
  * web site:    http://blueskynet.dyndns-server.com
  * e-mail  :    blueskyniss@gmail.com
@@ -44,7 +42,7 @@
  *              - 1U - Debug is enabled
  */
 #if !defined(CFG_DBG_ENABLE)
-# define CFG_DBG_ENABLE                 0u
+# define CFG_DBG_ENABLE                 1u
 #endif
 
 /**@brief       Enable/disable API arguments validation
@@ -55,7 +53,7 @@
  *              too.
  */
 #if !defined(CFG_DBG_API_VALIDATION)
-# define CFG_DBG_API_VALIDATION         1U
+# define CFG_DBG_API_VALIDATION         1u
 #endif
 
 /**@brief       Enable/disable internal checks
@@ -66,7 +64,7 @@
  *              too.
  */
 #if !defined(CFG_DBG_INTERNAL_CHECK)
-# define CFG_DBG_INTERNAL_CHECK         1U
+# define CFG_DBG_INTERNAL_CHECK         0u
 #endif
 
 /*================================*//** @cond *//*==  CONFIGURATION ERRORS  ==*/
