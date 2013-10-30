078dffe073a1ae0a1cba7adf5904038741d63537
Nenad Radulović <nenad.b.radulovic@gmail.com>
2013-10-30 10:57:10 +0100

    New IOCTL interface, probably will later add even more options

--- inc/drv/x_spi_ioctl.h
+++ inc/drv/x_spi_ioctl.h
@@ -33,10 +33,14 @@
 /*===============================================================  MACRO's  ==*/
 
 /*------------------------------------------------------------------------*//**
- * @name        IOctl magic number
+ * @name        Global settings
  * @{ *//*--------------------------------------------------------------------*/
 
 #define XSPI_IOC_MAGIC                  1
+
+/* --------------------------------------------------------------------------
+ * Current channel selection
+ * -------------------------------------------------------------------------- */
 
 enum xspiChn {
     XSPI_CHN_0                  = 0,
@@ -54,6 +58,10 @@ enum xspiChn {
  */
 #define XSPI_IOC_GET_CURRENT_CHN        _IOR(XSPI_IOC_MAGIC, 101, int)
 
+/* --------------------------------------------------------------------------
+ * FIFO channel selection
+ * -------------------------------------------------------------------------- */
+
 enum xspiFifoChn {
     XSPI_FIFO_CHN_DISABLED      = -1,
     XSPI_FIFO_CHN_0             = 0,
@@ -66,11 +74,15 @@ enum xspiFifoChn {
  * @details     -1 - disable FIFO
  *              0 - 3 channel with FIFO enabled
  */
-#define XSPI_IOC_SET_FIFO_MODE          _IOW(XSPI_IOC_MAGIC, 2, int)
+#define XSPI_IOC_SET_FIFO_CHN           _IOW(XSPI_IOC_MAGIC, 2, int)
 
 /**@brief       Get which channel has FIFO enabled
  */
-#define XSPI_IOC_GET_FIFO_MODE          _IOR(XSPI_IOC_MAGIC, 102, int)
+#define XSPI_IOC_GET_FIFO_CHN           _IOR(XSPI_IOC_MAGIC, 102, int)
+
+/* --------------------------------------------------------------------------
+ * CS pin mode
+ * -------------------------------------------------------------------------- */
 
 enum xspiCsMode {
     XSPI_CS_MODE_ENABLED        = 0,
@@ -87,8 +99,14 @@ enum xspiCsMode {
  */
 #define XSPI_IOC_GET_CS_MODE            _IOR(XSPI_IOC_MAGIC, 103, int)
 
-#define XSPI_MODE_MASTER                0
-#define XSPI_MODE_SLAVE                 1
+/* --------------------------------------------------------------------------
+ * SPI master/slave mode
+ * -------------------------------------------------------------------------- */
+
+enum xspiMode {
+    XSPI_MODE_MASTER            = 0,
+    XSPI_MODE_SLAVE             = 1
+};
 
 /**@brief       Set module master/slave mode
  * @details     0 - Master - The module generates the SPICLK and SPIEN[3:0]
@@ -98,28 +116,40 @@ enum xspiCsMode {
 
 /**@brief       Get module mode
  */
-#define XSPI_IOC_GET_MODE               _IOR(XSPI_IOX_MAGIC, 104, int)
+#define XSPI_IOC_GET_MODE               _IOR(XSPI_IOC_MAGIC, 104, int)
 
-/**@} *//*----------------------------------------------------------------*//**
- * @name        SPI data line configuration
- * @{ *//*--------------------------------------------------------------------*/
+/* --------------------------------------------------------------------------
+ * Single/multi channel mode
+ * -------------------------------------------------------------------------- */
 
-#define XSPI_PIN_LAYOUT_TX_RX           0
-#define XSPI_PIN_LAYOUT_RX_TX           1
+enum xspiChannelMode {
+    XSPI_CHANNEL_MODE_MULTI     = 0,
+    XSPI_CHANNEL_MODE_SINGLE    = 1
+};
 
-/**@brief       Reverse the directions of transmit/receive pins
- * @details     0 - pins are in transmit/receive layout
- *              1 - pins are in receive/transmit layout
+/**@brief       Set module channel mode
+ * @details     0 - More than one channel will be used in master mode
+ *              1 - Only one channel will be usedd in master mode
+ * @note        This setting is valid only if the device is in master mode (
+ *              XSPI_IOC_SET_MODE(XSPI_MODE_MASTER))
  */
-#define XSPI_IOC_SET_PIN_LAYOUT         _IOW(XSPI_IOC_MAGIC, 5, int)
+#define XSPI_IOC_SET_CHANNEL_MODE       _IOW(XSPI_IOC_MAGIC, 5, int)
 
-/**@brief       Gt the current pin layout
+/**@brief       Get module channel mode
  */
-#define XSPI_IOC_GET_PIN_LAYOUT         _IOR(XSPI_IOC_MAGIC, 105, int)
+#define XSPI_IOC_GET_CHANNEL_MODE       _IOR(XSPI_IOC_MAGIC, 105, int)
 
-/**@} *//*----------------------------------------------------------------*//**
- * @name        Timing tweaks
- * @{ *//*--------------------------------------------------------------------*/
+/* --------------------------------------------------------------------------
+ * Data initial delay
+ * -------------------------------------------------------------------------- */
+
+enum xspiInitialDelay {
+    XSPI_INITIAL_DELAY_0        = 0,
+    XSPI_INITIAL_DELAY_4        = 1,
+    XSPI_INITIAL_DELAY_8        = 2,
+    XSPI_INITIAL_DELAY_16       = 3,
+    XSPI_INITIAL_DELAY_32       = 4
+};
 
 /**@brief       First SPI word delayed
  * @details     0 - 32 clock cycles
@@ -130,110 +160,173 @@ enum xspiCsMode {
  */
 #define XSPI_IOC_GET_INITIAL_DELAY      _IOR(XSPI_IOC_MAGIC, 106, int)
 
-#define XSPI_CS_DELAY_0                 0
-#define XSPI_CS_DELAY_1                 1
-#define XSPI_CS_DELAY_2                 2
-#define XSPI_CS_DELAY_3                 3
-
-/**@brief       Chip-Select Timing Control
- * @details     0 - 3 cycle delay
- * @note        This setting is valid only when XSPI_IOC_SET_CS_MODE = 0.
- */
-#define XSPI_IOC_SET_CS_DELAY           _IOW(XSPI_IOC_MAGIC, 7, int)
-
-/**@brief       Get Chip-select timing
- */
-#define XSPI_IOC_GET_CS_DELAY           _IOR(XSPI_IOC_MAGIC, 107, int)
-
 /**@} *//*----------------------------------------------------------------*//**
- * @name        SPI Transfer format
- * @brief       All IOC data/control is per channel
+ * @name        Per channel settings
  * @{ *//*--------------------------------------------------------------------*/
 
-/**@brief       Set data word length
- * @details     Data length can be in range from 4 bits to 32 bits
- */
-#define XSPI_IOC_SET_WORD_LENGTH        _IOW(XSPI_IOC_MAGIC, 8, int)
+/* --------------------------------------------------------------------------
+ * Transfer mode
+ * -------------------------------------------------------------------------- */
 
-/**@brief       Get data word length
- */
-#define XSPI_IOC_GET_WORD_LENGTH        _IOR(XSPI_IOC_MAGIC, 108, int)
-
-#define XSPI_CS_POLARITY_ACTIVE_HIGH    0
-#define XSPI_CS_POLAROTY_ACTIVE_LOW     1
-
-/**@brief       Set Chip-Select polarity
- * @details     0 - CS is held high during the active state
- *              1 - CS is held low during the active state
- */
-#define XSPI_IOC_SET_CS_POLARITY        _IOW(XSPI_IOC_MAGIC, 9, int)
-
-/**@brief       Get Chip-Select polarity
- */
-#define XSPI_IOC_GET_CS_POLARITY        _IOR(XSPI_IOC_MAGIC, 109, int)
-
-#define XSPI_CS_STATE_ACTIVE            1
-#define XSPI_CS_STATE_INACTIVE          0
-
-/**@brief       Set Chip-Select pin state
- * @details     0 - Set CS state to inactive
- *              1 - Set CS state to active
- * @note        This setting is valid only when XSPI_IOC_SET_CS_MODE is set to 0.
- */
-#define XSPI_IOC_SET_CS_STATE           _IOW(XSPI_IOC_MAGIC, 10, int)
-
-/**@brief       Get Chip-Select pin state
- */
-#define XSPI_IOC_GET_CS_STATE           _IOR(XSPI_IOC_MAGIC, 110, int)
-
-/**@brief       Define SPICLK clock frequency
- */
-#define XSPI_IOC_SET_CLOCK_FREQ         _IOW(XSPI_IOC_MAGIC, 11, int)
-
-/**@brief       Get SPICLK clock frequency
- */
-#define XSPI_IOC_GET_CLOCK_FREQ         _IOR(XSPI_IOC_MAGIC, 111, int)
-
-#define XSPI_CLOCK_PHASE_ODD_EDGES      0
-#define XSPI_CLOCK_PHASE_EVEN_EDGES     1
-
-/**@brief       Define SPICLK clock phase
- * @details     0 - Data is latched on odd numbered edges of SPICLK
- *              1 - Data is latched on even numbered edges of SPICLK
- */
-#define XSPI_IOC_SET_CLOCK_PHASE        _IOW(XSPI_IOC_MAGIC, 12, int)
-
-/**@brief       Get SPICLK clock phase
- */
-#define XSPI_IOC_GET_CLOCK_PHASE        _IOR(XSPI_IOC_MAGIC, 112, int)
-
-#define XSPI_CLOCK_POLARITY_ACTIVE_HIGH 0
-#define XSPI_CLOCK_POLARITY_ACTIVE_LOW  1
-
-/**@brief       Define SPICLK clock polarity
- * @details     0 - SPICLK is held high during the active state
- *              1 - SPICLK is held low during the active state
- */
-#define XSPI_IOC_SET_CLOCK_POLARITY     _IOW(XSPI_IOC_MAGIC, 13, int)
-
-/**@brief       Get SPICLK clock polarity
- */
-#define XSPI_IOC_GET_CLOCK_POLARITY     _IOR(XSPI_IOC_MAGIC, 113, int)
-
-#define XSPI_TRANSFER_MODE_TX_AND_RX    0
-#define XSPI_TRANSFER_MODE_RX           1
-#define XSPI_TRANSFER_MODE_TX           2
+enum xspiTransferMode {
+    XSPI_TRANSFER_MODE_TX_AND_RX = 0,
+    XSPI_TRANSFER_MODE_RX_ONLY   = 1,
+    XSPI_TRANSFER_MODE_TX_ONLY   = 2
+};
 
 /**@brief       Transmit/receive modes
  * @details     0 - Transmit and receive mode
  *              1 - Receive mode only
  *              2 - Transmit mode only
  */
-#define XSPI_IOC_SET_TRANSFER_MODE      _IOW(XSPI_IOC_MAGIC, 14, int)
+#define XSPI_IOC_SET_TRANSFER_MODE      _IOW(XSPI_IOC_MAGIC, 7, int)
 
 /**@brief       Get transmit/receive mode
  */
-#define XSPI_IOC_GET_TRANSFER_MODE      _IOR(XSPI_IOC_MAGIC, 114, int)
+#define XSPI_IOC_GET_TRANSFER_MODE      _IOR(XSPI_IOC_MAGIC, 107, int)
+
+/* --------------------------------------------------------------------------
+ * Tx/Rx pin layout
+ * -------------------------------------------------------------------------- */
+
+enum xspiPinLayout {
+    XSPI_PIN_LAYOUT_TX_RX       = 0,
+    XSPI_PIN_LAYOUT_RX_TX       = 1
+};
+
+/**@brief       Reverse the directions of transmit/receive pins
+ * @details     0 - pins are in transmit/receive layout
+ *              1 - pins are in receive/transmit layout
+ */
+#define XSPI_IOC_SET_PIN_LAYOUT         _IOW(XSPI_IOC_MAGIC, 8, int)
+
+/**@brief       Gt the current pin layout
+ */
+#define XSPI_IOC_GET_PIN_LAYOUT         _IOR(XSPI_IOC_MAGIC, 108, int)
+
+/* --------------------------------------------------------------------------
+ * Data word length
+ * -------------------------------------------------------------------------- */
+
+/**@brief       Set data word length
+ * @details     Data length can be in range from 4 bits to 32 bits
+ */
+#define XSPI_IOC_SET_WORD_LENGTH        _IOW(XSPI_IOC_MAGIC, 9, int)
+
+/**@brief       Get data word length
+ */
+#define XSPI_IOC_GET_WORD_LENGTH        _IOR(XSPI_IOC_MAGIC, 109, int)
+
+/* --------------------------------------------------------------------------
+ * CS delay
+ * -------------------------------------------------------------------------- */
+
+enum xspiCsDelay {
+    XSPI_CS_DELAY_0_5           = 0,
+    XSPI_CS_DELAY_1_5           = 1,
+    XSPI_CS_DELAY_2_5           = 2,
+    XSPI_CS_DELAY_3_5           = 3
+};
+
+/**@brief       Chip-Select Timing Control (per channel setting)
+ * @details     0 - 3 cycle delay
+ * @note        This setting is valid only when XSPI_IOC_SET_CS_MODE = 0.
+ */
+#define XSPI_IOC_SET_CS_DELAY           _IOW(XSPI_IOC_MAGIC, 10, int)
+
+/**@brief       Get Chip-select timing
+ */
+#define XSPI_IOC_GET_CS_DELAY           _IOR(XSPI_IOC_MAGIC, 110, int)
+
+/* --------------------------------------------------------------------------
+ * CS polarity
+ * -------------------------------------------------------------------------- */
+
+enum xspiCsPolarity {
+    XSPI_CS_POLARITY_ACTIVE_HIGH = 0,
+    XSPI_CS_POLAROTY_ACTIVE_LOW  = 1
+};
+
+/**@brief       Set Chip-Select polarity
+ * @details     0 - CS is held high during the active state
+ *              1 - CS is held low during the active state
+ */
+#define XSPI_IOC_SET_CS_POLARITY        _IOW(XSPI_IOC_MAGIC, 11, int)
+
+/**@brief       Get Chip-Select polarity
+ */
+#define XSPI_IOC_GET_CS_POLARITY        _IOR(XSPI_IOC_MAGIC, 111, int)
+
+/* --------------------------------------------------------------------------
+ * CS state
+ * -------------------------------------------------------------------------- */
+
+enum xspiCsState {
+    XSPI_CS_STATE_INACTIVE      = 0,
+    XSPI_CS_STATE_ACTIVE        = 1
+};
+
+/**@brief       Set-force Chip-Select pin state between SPI words
+ * @details     0 - Set CS state to inactive
+ *              1 - Set CS state to active
+ * @note        This setting is valid only when XSPI_IOC_SET_CS_MODE is set to 0.
+ */
+#define XSPI_IOC_SET_CS_STATE           _IOW(XSPI_IOC_MAGIC, 12, int)
+
+/**@brief       Get Chip-Select pin state
+ */
+#define XSPI_IOC_GET_CS_STATE           _IOR(XSPI_IOC_MAGIC, 112, int)
+
+/* --------------------------------------------------------------------------
+ * Clock frrequency
+ * -------------------------------------------------------------------------- */
+
+/**@brief       Define SPICLK clock frequency
+ */
+#define XSPI_IOC_SET_CLOCK_FREQ         _IOW(XSPI_IOC_MAGIC, 13, int)
+
+/**@brief       Get SPICLK clock frequency
+ */
+#define XSPI_IOC_GET_CLOCK_FREQ         _IOR(XSPI_IOC_MAGIC, 113, int)
+
+/* --------------------------------------------------------------------------
+ * Clock phase
+ * -------------------------------------------------------------------------- */
+
+enum xspiClockPhase {
+    XSPI_CLOCK_PHASE_ODD_EDGES  = 0,
+    XSPI_CLOCK_PHASE_EVEN_EDGES = 1
+};
+
+/**@brief       Define SPICLK clock phase
+ * @details     0 - Data is latched on odd numbered edges of SPICLK
+ *              1 - Data is latched on even numbered edges of SPICLK
+ */
+#define XSPI_IOC_SET_CLOCK_PHASE        _IOW(XSPI_IOC_MAGIC, 14, int)
+
+/**@brief       Get SPICLK clock phase
+ */
+#define XSPI_IOC_GET_CLOCK_PHASE        _IOR(XSPI_IOC_MAGIC, 114, int)
+
+/* --------------------------------------------------------------------------
+ * Clock polarity
+ * -------------------------------------------------------------------------- */
+
+enum xspiClockPolarity {
+    XSPI_CLOCK_POLARITY_ACTIVE_HIGH = 0,
+    XSPI_CLOCK_POLARITY_ACTIVE_LOW  = 1
+};
+
+/**@brief       Define SPICLK clock polarity
+ * @details     0 - SPICLK is held high during the active state
+ *              1 - SPICLK is held low during the active state
+ */
+#define XSPI_IOC_SET_CLOCK_POLARITY     _IOW(XSPI_IOC_MAGIC, 15, int)
+
+/**@brief       Get SPICLK clock polarity
+ */
+#define XSPI_IOC_GET_CLOCK_POLARITY     _IOR(XSPI_IOC_MAGIC, 115, int)
+
 
 /**@} *//*----------------------------------------------------------------*//**
  * @name        SPI Status
@@ -242,11 +335,11 @@ enum xspiCsMode {
 
 /**@brief       Get the device status
  */
-#define XSPI_IOC_GET_STATUS             _IOR(XSPI_IOC_MAGIC, 115, int)
+#define XSPI_IOC_GET_STATUS             _IOR(XSPI_IOC_MAGIC, 200, int)
 
 /**@brief       Get the channel status
  */
-#define XSPI_IOC_GET_CHN_STATUS         _IOR(XSPI_IOC_MAGIC, 116, int)
+#define XSPI_IOC_GET_CHN_STATUS         _IOR(XSPI_IOC_MAGIC, 201, int)
 
 /**@} *//*--------------------------------------------------------------------*/
 
